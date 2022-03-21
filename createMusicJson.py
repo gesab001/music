32d3bbe84be51764869390e62fd7a650981ed06a
giovanni <144000giovanni@gmail.com>
2022-03-22 12:44:03 +1300

    added gentle shepherd

--- createMusicJson.py
+++ createMusicJson.py
@@ -14,7 +14,7 @@ with open("musiclist2.json", "w") as outfile:
   
 """
 
-f = open("musiclist2.json", "r")
+f = open("empty_alphabetical_list.json", "r")
 musiclist2 = json.loads(f.read())
 f.close()
 
@@ -23,14 +23,32 @@ f = open("musiclist.json", "r")
 musiclist = json.loads(f.read())
 f.close()
 
+count = 0
 for x in musiclist:
   filename = x
+  print("filename: " + filename)
+  count = count + 1
   title = musiclist[x]
+  print("title: " + title)
   firstLetter = title[0:1]
   print(firstLetter)
-  musiclist2[firstLetter].append({"filename": filename, "title": title})
+  musiclist2["items"][firstLetter].append({"filename": filename, "title": title})
 
+musiclist2["totalItems"] = count
 print(musiclist2)
-with open("musiclist3.json", "w") as outfile:
+
+songIndex = 1
+items = musiclist2["items"]
+print(items)
+for letter in items:
+   songs = items[letter]
+   if len(songs)>0:
+     for x in range(0, len(songs)):
+       print(songIndex)
+       musiclist2["items"][letter][x]["songIndex"] = songIndex
+       print(musiclist2["items"][letter][x])
+       songIndex+=1
+       
+with open("musiclist2.json", "w") as outfile:
   json.dump(musiclist2, outfile, indent=4, sort_keys=True)
   
