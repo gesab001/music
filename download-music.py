fcb7d90641fb7462c28e456e7174cfd21e1522f2
giovanni <144000giovanni@gmail.com>
2022-03-26 13:52:08 +1300

    added i cannot tell why

--- download-music.py
+++ download-music.py
@@ -4,6 +4,6 @@ import subprocess
 
 youtubeId = input("youtube url/id: ")
 command = "youtube-dl --extract-audio --audio-format mp3 --audio-quality 0 " + youtubeId 
-savecommand = " && py listmusicfiles.py"
+savecommand = " && py listmusicfiles.py && createMusicJson.py"
 subprocess.call(command + savecommand, shell=True)
 
